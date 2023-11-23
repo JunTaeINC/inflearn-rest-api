2dff9cf695c6adbbd0cf77a7af9c785ae67abe33
JunTaeINC <juntea3523@naver.com>
2023-11-23 15:17:06 +0900

    feat : POST /api/events returns 201

--- src/main/java/hello/study/restapi/event/Event.java
+++ src/main/java/hello/study/restapi/event/Event.java
@@ -1,5 +1,11 @@
 package hello.study.restapi.event;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,11 +17,14 @@ import lombok.Setter;
 @Builder
 @Getter
 @Setter
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Event {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String description;
@@ -24,9 +33,17 @@ public class Event {
 	private LocalDateTime beginEventDateTime;
 	private LocalDateTime endEventDateTime;
 	private String location;
+	private int basePrice;
 	private int maxPrice;
 	private int limitOfEnrollment;
 	private boolean offline;
 	private boolean free;
-	private EventStatus eventStatus = EventStatus.DRAFT;
+	/*
+	  Enumerated의 기본값은 EnumType.ORDINAL 인데 ORDINAL은 Enum의 순서에 따라서 index 값을 데이터베이스에 저장한다.
+	  만약, Enum의 순서가 바뀐다거나 추가로 입력이 될경우 데이터의 정합성 문제를 야기할수있다. 그래서 String Type으로 저장하는게 더 좋다.
+
+	  단점 : ORDINAL에 비해 약간 더 많은 저장 공간을 차지하고, 문자열 비교가 숫자 비교보다는 느리다
+	 */
+	@Enumerated(EnumType.STRING)
+	private EventStatus eventStatus;
 }
--- src/test/java/hello/study/restapi/event/EventControllerTest.java
+++ src/test/java/hello/study/restapi/event/EventControllerTest.java
@@ -1,12 +1,18 @@
 package hello.study.restapi.event;
 
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -19,12 +25,36 @@ public class EventControllerTest {
 	@Autowired
 	MockMvc mockMvc;
 
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@MockBean
+	EventRepository eventRepository;
+
 	@Test
 	void createEvent() throws Exception {
-		mockMvc.perform(post("/api/events/")
+		Event event = Event.builder()
+			.name("Kim's")
+			.description("HBD Party")
+			.beginEnrollmentDateTime(LocalDateTime.of(2023, 11, 16, 0, 0))
+			.closeEnrollmentDateTime(LocalDateTime.of(2023, 11, 16, 23, 59))
+			.beginEventDateTime(LocalDateTime.of(2023, 11, 17, 23, 59))
+			.endEventDateTime(LocalDateTime.of(2023, 11, 18, 23, 59))
+			.basePrice(10_000)
+			.maxPrice(100_000)
+			.location("Kim's House")
+			.build();
+		event.setId(1);
+
+		Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+		mockMvc.perform(post("/api/events")
 				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaTypes.HAL_JSON) // accept Header
-			)
-			.andExpect(status().isCreated()); // isCreated == 201
+				.accept(MediaTypes.HAL_JSON_VALUE) // accept Header
+				.content(objectMapper.writeValueAsString(event)))
+			.andDo(print()) // show Result
+			.andExpect(status().isCreated()) // isCreated == 201
+			.andExpect(jsonPath("id").exists())
+		;
 	}
 }
