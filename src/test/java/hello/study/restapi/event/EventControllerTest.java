7dbc1609aa24c2421abaff3d4f759ba288043beb
JunTaeINC <juntea3523@naver.com>
2023-11-29 20:28:55 +0900

    feat : Make Spring Rest Docs index.html

--- src/test/java/hello/study/restapi/event/EventControllerTest.java
+++ src/test/java/hello/study/restapi/event/EventControllerTest.java
@@ -28,12 +28,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
+@ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
 public class EventControllerTest {
